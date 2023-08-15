0af6604e2f2f51a783906b7597a2fa9b1955331f
headzoo <sean@headzoo.io>
2023-08-14 21:15:56 -0400

    Code cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import './config';
 import { createDatabase } from './database';
 import { runDiscuitWatch } from './discuit';
-// import { runAdminSite } from './server';
 
 (async () => {
   await createDatabase();
   await runDiscuitWatch();
-  // await runAdminSite();
 })();
